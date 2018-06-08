35e6999071d1ad6c351a880a88c668b216fcdaf6
Headlight <1360081795@qq.com>
2018-06-08 13:32:47 +0800

    13
    文件上传

---
+++ Spring boot6-2/src/main/java/com/zll/Exception/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.zll.Exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.multipart.MultipartException;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+/**
+ * Created by zll on 2018/4/22 0022.
+ */
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    //http://jira.spring.io/browse/SPR-14651
+    //4.3.5 supports RedirectAttributes redirectAttributes
+    @ExceptionHandler(MultipartException.class)
+    public String handleError1(MultipartException e, RedirectAttributes redirectAttributes) {
+
+        redirectAttributes.addFlashAttribute("message", e.getCause().getMessage());
+        return "redirect:/uploadStatus";
+
+    }
+
+}
